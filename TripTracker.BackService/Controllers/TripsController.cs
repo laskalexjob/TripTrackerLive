20b697d316c6cbd96abccf41ee94c7657f0bc4b1
laskalexjob <laskalexjob@gmail.com>
2019-02-03 20:14:48 +0300

    Main EF configuration

---
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TripTracker.BackService.Data;
+using TripTracker.BackService.Models;
+
+namespace TripTracker.BackService.Controllers
+{
+    [Route("api/[controller]")]
+    public class TripsController : Controller
+    {
+        private TripContext _context;
+
+        public TripsController(TripContext context)
+        {
+            _context = context;
+        }
+        
+        // GET api/Trips
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return _context.Trips.ToList();
+        }
+
+        // GET api/Trips/5
+        [HttpGet("{id}")]
+        public Trip Get(int id)
+        {
+            return _context.Get(id);
+        }
+
+        // POST api/Trips
+        [HttpPost]
+        public void Post([FromBody]Trip trip)
+        {
+            _context.Add(trip);
+        }
+
+        // PUT api/Trips/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Trip trip)
+        {
+            _context.Update(trip);
+        }
+
+        // DELETE api/Trips/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _context.Remove(id);
+        }
+    }
+}
---
+++ TripTracker.BackService/Models/Repository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TripTracker.BackService.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>
+        {
+            new Trip
+            {
+                Id = 1,
+                Name = "Mvp summit",
+                StartDate = new DateTime(2018,3,5),
+                EndDate = new DateTime(2018, 3, 8)
+            },
+            new Trip
+            {
+                Id = 2,
+                Name = "Dev intersection",
+                StartDate = new DateTime(2018,3,27),
+                EndDate = new DateTime(2018,3,28)
+            },
+            new Trip
+            {
+                Id = 3,
+                Name = "Build 2018",
+                StartDate = new DateTime(2018,5,28),
+                EndDate = new DateTime(2018, 5,29)
+            }
+        };
+
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+
+        public Trip Get(int id)
+        {
+            return MyTrips.First(t => t.Id == id);
+        }
+
+        public void Add(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update(Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            MyTrips.Add(tripToUpdate);
+        }
+
+        public void Remove(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == id));
+        }
+    }
+}
